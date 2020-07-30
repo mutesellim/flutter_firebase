7cb5928add9b3fe8bd814a2192edfdcc943f54fc
Mehmet <mbasik95@gmail.com>
2020-07-30 10:18:29 +0300

    30Temmuz

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Flutter Firebase Demo",
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: FirestoreIslemleri(),
+      home: MyHomePage(),
     );
   }
 }
@@ -24,6 +24,29 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(color: Colors.deepOrange,
+                child: Text("Firestore İşlemlerine Git",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 20),),
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => FirestoreIslemleri())),
+              ),
+              RaisedButton(color: Colors.lightBlue,
+                child: Text("Firebase İşlemlerine Git",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 20),),
+                onPressed: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LoginIslemleri())),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
